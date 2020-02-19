4f6266e089b18eef32bf2eac61a7659f21985344
linux_china <libing.chen@gmail.com>
2020-02-19 10:39:42 -0800

    Polishing

--- src/com/wasmplugin/WasmLineMarkerProvider.java
+++ src/com/wasmplugin/WasmLineMarkerProvider.java
@@ -28,9 +28,9 @@ public class WasmLineMarkerProvider extends LineMarkerProviderDescriptor {
     @Override
     public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement psiElement) {
         if (psiElement instanceof WasmExport) {
-            return new LineMarkerInfo(psiElement, psiElement.getTextRange(), AllIcons.Actions.Export, null, null, GutterIconRenderer.Alignment.CENTER);
+            return new LineMarkerInfo(psiElement, psiElement.getTextRange(), AllIcons.ToolbarDecorator.Export, null, null, GutterIconRenderer.Alignment.CENTER);
         } else if (psiElement instanceof WasmImport) {
-            return new LineMarkerInfo(psiElement, psiElement.getTextRange(), AllIcons.Css.Import, null, null, GutterIconRenderer.Alignment.CENTER);
+            return new LineMarkerInfo(psiElement, psiElement.getTextRange(), AllIcons.ToolbarDecorator.Import, null, null, GutterIconRenderer.Alignment.CENTER);
         }
         return null;
     }
